f03a6802ff11530211381afd6091bac40451ad84
Venkata chandrasekhar Nainala <vcnainala@gmail.com>
2021-07-20 11:20:11 +0100

    Init commit

---
+++ docs/intro.md
@@ -0,0 +1,36 @@
+---
+sidebar_position: 1
+---
+
+# Tutorial Intro for nmrXiv
+
+Let's discover **Docusaurus in less than 5 minutes**.
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+## Getting Started
+
+Get started by **creating a new site**.
+
+Or **try Docusaurus immediately** with **[new.docusaurus.io](https://new.docusaurus.io)**.
+
+## Generate a new site
+
+Generate a new Docusaurus site using the **classic template**:
+
+```shell
+npx @docusaurus/init@latest init my-website classic
+```
+
+## Start your site
+
+Run the development server:
+
+```shell
+cd my-website
+
+npx docusaurus start
+```
+
+Your site starts at `http://localhost:3000`.
+
+Open `docs/intro.md` and edit some lines: the site **reloads automatically** and display your changes.
---
+++ docusaurus.config.js
@@ -0,0 +1,161 @@
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+module.exports = {
+    // i18n: {
+    //     defaultLocale: 'en',
+    //     locales: ['en', 'fr'],
+    // },
+    title: 'nmrXiv',
+    tagline: 'Highly visible, and consensus-driven NMR data repository and computational platform',
+    url: 'https://nmrxiv.org',
+    baseUrl: '/',
+    onBrokenLinks: 'throw',
+    onBrokenMarkdownLinks: 'warn',
+    favicon: 'img/favicon.ico',
+    organizationName: '', // Usually your GitHub org/user name.
+    projectName: 'nmrXiv - Docs', // Usually your repo name.
+    themeConfig: {
+        announcementBar: {
+            id: 'support_us', // Any value that will identify this message.
+            content: 'We are looking to revamp our docs, please provide your feedback / suggestions <a target="_blank" rel="noopener noreferrer" href="mailto:info@nmrxiv.org?subject=nmrXiv feedback/suggestions&body=Hi,<Add your suggestions here>">here</a>',
+            backgroundColor: '#fafbfc', // Defaults to `#fff`.
+            textColor: '#091E42', // Defaults to `#000`.
+            isCloseable: true, // Defaults to `true`.
+        },
+        colorMode: {
+            // "light" | "dark"
+            defaultMode: 'light',
+
+            // Hides the switch in the navbar
+            // Useful if you want to support a single color mode
+            disableSwitch: false,
+
+            // Should we use the prefers-color-scheme media-query,
+            // using user system preferences, instead of the hardcoded defaultMode
+            respectPrefersColorScheme: false,
+
+            // Dark/light switch icon options
+            switchConfig: {
+                // Icon for the switch while in dark mode
+                darkIcon: '🌙',
+
+                // CSS to apply to dark icon,
+                // React inline style object
+                // see https://reactjs.org/docs/dom-elements.html#style
+                darkIconStyle: {
+                    marginLeft: '0px',
+                },
+
+                // Unicode icons such as '\u2600' will work
+                // Unicode with 5 chars require brackets: '\u{1F602}'
+                lightIcon: '\u2600',
+
+                lightIconStyle: {
+                    marginLeft: '1px',
+                },
+            },
+        },
+        navbar: {
+            title: '',
+            logo: {
+                alt: 'nmrXiv - Docs',
+                src: 'img/logo.svg',
+            },
+            items: [
+                // {
+                //     type: 'localeDropdown',
+                //     position: 'right',
+                // },
+                // {
+                //     type: 'doc',
+                //     docId: 'intro',
+                //     position: 'left',
+                //     label: 'DOCS',
+                // },
+            ],
+        },
+        footer: {
+            style: 'dark',
+            links: [
+                // {
+                //     title: 'Docs',
+                //     items: [{
+                //         label: 'DOCS',
+                //         to: '/docs/intro',
+                //     }, ],
+                // },
+                // {
+                //     title: 'Community',
+                //     items: [{
+                //             label: 'Stack Overflow',
+                //             href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+                //         },
+                //         {
+                //             label: 'Discord',
+                //             href: 'https://discordapp.com/invite/docusaurus',
+                //         },
+                //         {
+                //             label: 'Twitter',
+                //             href: 'https://twitter.com/docusaurus',
+                //         },
+                //     ],
+                // },
+                // {
+                //     title: 'More',
+                //     items: [
+                //         // {
+                //         //     label: 'Blog',
+                //         //     to: '/blog',
+                //         // },
+                //         {
+                //             label: 'GitHub',
+                //             href: 'https://github.com/facebook/docusaurus',
+                //         },
+                //     ],
+                // },
+            ],
+            copyright: `© nmrXiv ${new Date().getFullYear()}. Built with Docusaurus.`,
+        },
+        prism: {
+            theme: lightCodeTheme,
+            darkTheme: darkCodeTheme,
+        },
+        algolia: {
+            apiKey: 'YOUR_API_KEY',
+            indexName: 'YOUR_INDEX_NAME',
+
+            // Optional: see doc section below
+            contextualSearch: true,
+
+            // Optional: see doc section below
+            appId: 'YOUR_APP_ID',
+
+            // Optional: Algolia search parameters
+            searchParameters: {},
+
+            //... other Algolia params
+        },
+    },
+    presets: [
+        [
+            '@docusaurus/preset-classic',
+            {
+                docs: {
+                    sidebarPath: require.resolve('./sidebars.js'),
+                    // Please change this to your repo.
+                    editUrl: 'https://github.com/NFDI4Chem/nmrxiv-docs/edit/master/website/',
+                },
+                // blog: {
+                //     showReadingTime: true,
+                //     // Please change this to your repo.
+                //     editUrl: 'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+                // },
+                theme: {
+                    customCss: require.resolve('./src/css/custom.css'),
+                },
+            },
+        ],
+    ],
+};
