a3cd680d0234f02e6178ee86bb0280e8f9ae23ac
Venkata chandrasekhar Nainala <vcnainala@gmail.com>
2021-08-25 11:42:35 +0100

    Updated home page and other docs changes

--- src/components/HomepageFeatures.js
+++ src/components/HomepageFeatures.js
@@ -2,63 +2,74 @@ import React from 'react';
 import clsx from 'clsx';
 import styles from './HomepageFeatures.module.css';
 
-const FeatureList = [
-  {
-    title: 'Easy to Use',
-    Svg: require('../../static/img/undraw_docusaurus_mountain.svg').default,
-    description: (
-      <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
-      </>
-    ),
-  },
-  {
-    title: 'Focus on What Matters',
-    Svg: require('../../static/img/undraw_docusaurus_tree.svg').default,
-    description: (
-      <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
-      </>
-    ),
-  },
-  {
-    title: 'Powered by React',
-    Svg: require('../../static/img/undraw_docusaurus_react.svg').default,
-    description: (
-      <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
-      </>
-    ),
-  },
+const FeatureList = [{
+        title: 'Voluntarily FAIR',
+        Svg: require('../../static/img/fair.svg').default,
+        description: ( <
+            >
+            nmrXiv is a cloud - based infrastructure that uses freely shared source code and deployment methods.Application of industry - grade development practices to ensure platform reliability,
+            safety, and efficacy. <
+            />
+        ),
+    },
+    {
+        title: 'Curation and analysis standards',
+        Svg: require('../../static/img/standards.svg').default,
+        description: ( <
+            >
+            Standards
+            for data and metadata will be non -
+            prescriptivist by nature.Standards will be built with input and
+
+            contributions from the community and made compatible with existing and potential newly developed formats and approaches. < / >
+        ),
+    },
+    {
+        title: 'Community Engagement',
+        Svg: require('../../static/img/community.svg').default,
+        description: ( <
+            >
+            Engagement of the Natural Product and adjacent communities through webinars,
+            workshops, tutorials, and community calls will ensure that nmrXiv meets the researchers’ needs and implements adequate incentive mechanisms
+            for data contributions and curation. <
+            />
+        ),
+    },
 ];
 
-function Feature({Svg, title, description}) {
-  return (
-    <div className={clsx('col col--4')}>
-      <div className="text--center">
-        <Svg className={styles.featureSvg} alt={title} />
-      </div>
-      <div className="text--center padding-horiz--md">
-        <h3>{title}</h3>
-        <p>{description}</p>
-      </div>
-    </div>
-  );
+function Feature({ Svg, title, description }) {
+    return ( <
+        div className = { clsx('col col--4') } >
+        <
+        div className = "text--center" >
+        <
+        Svg className = { styles.featureSvg }
+        alt = { title }
+        /> < /
+        div > <
+        div className = "text--center padding-horiz--md" >
+        <
+        h3 > { title } < /h3> <
+        p > { description } < /p> < /
+        div > <
+        /div>
+    );
 }
 
 export default function HomepageFeatures() {
-  return (
-    <section className={styles.features}>
-      <div className="container">
-        <div className="row">
-          {FeatureList.map((props, idx) => (
-            <Feature key={idx} {...props} />
-          ))}
-        </div>
-      </div>
-    </section>
-  );
+    return ( <
+        section className = { styles.features } >
+        <
+        div className = "container" >
+        <
+        div className = "row" > {
+            FeatureList.map((props, idx) => ( <
+                Feature key = { idx } {...props }
+                />
+            ))
+        } <
+        /div> < /
+        div > <
+        /section>
+    );
 }
